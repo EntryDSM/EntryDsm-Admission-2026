eea238622c62b1160ddf3084baa6e85fd97f4cd7
강세아 <gangsea817@gmail.com>
2026-07-28 22:16:59 +0900

    🐛 :: AuthLink color 수정 및 App.tsx 스타일 수정
    🐛 :: AuthLink color 수정 및 App.tsx 스타일 수정

--- apps/entry-auth/src/App.tsx
+++ apps/entry-auth/src/App.tsx
@@ -1,8 +1,16 @@
 import { RouterProvider } from "react-router";
+import { ToastContainer } from "react-toastify";
+import { GlobalStyle } from "@entry/design";
 import { Router } from "./router";
 
 const App = () => {
-  return <RouterProvider router={Router} />;
+  return (
+    <>
+      <RouterProvider router={Router} />
+      <ToastContainer />
+      <GlobalStyle />
+    </>
+  );
 };
 
 export default App;
